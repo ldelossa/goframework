68041a4c6d7a3492fe0eebcd99b74ec46c6fd66b
Louis Delossantos <ldelossa.ld@gmail.com>
2019-09-30 16:29:51 -0400

    add codes

---
+++ http/code.go
@@ -0,0 +1,10 @@
+package http
+
+const (
+	CodeCreateFail           = "create-resource-fail"
+	CodeSuccess              = "success"
+	CodeInternalServerError  = "internal-server-error"
+	CodeMethodNotImplemented = "method-not-implemented"
+	CodeFailedSerialization  = "failed-serialization"
+	CodeNotFound             = "resource-not-found"
+)
